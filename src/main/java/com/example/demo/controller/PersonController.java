fe3ce6138e72865c2c93e0be3802388a48359ae4
Muhammad Dava Fahreza <mdavafahreza05@gmail.com>
2025-01-09 16:07:49 +0700

    feat: add feature controller

--- src/main/java/com/example/demo/controller/PersonController.java
+++ src/main/java/com/example/demo/controller/PersonController.java
@@ -1,29 +1,29 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo.model.Person;
 import com.example.demo.repo.PersonRepo;
 
 @RestController
 public class PersonController {
 
     @Autowired
-    private PersonRepo PersonRepo;
+    PersonRepo repo;
 
     @PostMapping("/addPerson")
-    public String addPerson() {
-        // Add logic to save a new person to the database
-        return "Person added successfully";
+    public void addPerson(@RequestBody Person person) {
+        repo.save(person);
     }
 
     @GetMapping("/Persons")
     public Iterable<Person> getAllPersons() {
         // Mengembalikan semua data Person dalam format JSON
-        return PersonRepo.findAll();
+        return repo.findAll();
     }
 
 }
